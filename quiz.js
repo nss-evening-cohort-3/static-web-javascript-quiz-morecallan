05771fd43f7f7065ede69dd9ac31800508571770
Callan Morrison <callan.l.morrison@gmail.com>
2016-02-11 19:32:47 -0600

    added functions to build tree

--- quiz.js
+++ quiz.js
@@ -1,17 +1,28 @@
-var treeTrial = document.getElementById("tree");
-
-var user = prompt("Enter the height of your tree.")
-var userInput = parseInt(user);
-
-var character = prompt("Enter the character you want to make a tree out of.")
-
 var width;
+var treeDiv = document.getElementById("tree")
+treeDiv.style.textAlign="center";
 
-for (var i = 0; i < userInput; i++) {
-    width = (i*2) + 1;
-    treeTrial.innerHTML += "<p>";
-    for (var j = 0; j < width; j++) {
-        treeTrial.innerHTML += character;
-    }
-    treeTrial.innerHTML += "</p>";
+
+function submitInfo() {
+    var formTree = {
+    height: parseInt(document.getElementById("height").value),
+    symbol: document.getElementById("symbol").value     
+    };
+    console.log("formTree", formTree);
+
+    buildTree(formTree);
+    return formTree;
 }
+
+
+function buildTree(tree) {
+    for (var i = 0; i < tree.height; i++) {
+        width = (i*2) + 1;
+        treeDiv.innerHTML += "<p>";
+        for (var j = 0; j < width; j++) {
+            treeDiv.innerHTML += tree.symbol;
+        }
+        treeDiv.innerHTML += "</p>";
+    }
+}       
+
